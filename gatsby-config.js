e0bbb3686fb2e7d1daa45c9432b8c7ab464621dc
Tedi Ace <43319929+Tediace@users.noreply.github.com>
2021-10-18 03:29:18 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Derstern",
+    description: "Official Contentful SAP by Gatsby",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
